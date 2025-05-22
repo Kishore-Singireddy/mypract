735b2b9f9f61fa0d2dde643041560313ac90491d
Kishore Singireddy <official4kishore@gmail.com>
2025-05-22 16:58:22 +0530

    logs

---
+++ colours.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Enabling colours
+
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+C='\e[0m'
+
+echo -e "This is ${R} RED ${C}"
+echo -e "This is ${G} GREEN ${C}"
+echo -e "This is ${Y} YELLOW ${C}"
+echo -e "This is ${B} BLUE ${C}"
+echo -e "This is ${C} No colour ${C}"
+
