91be9277556bcf25ca881f829cbfccfecb6e9bfe
Kishore Singireddy <official4kishore@gmail.com>
2025-05-22 13:28:35 +0530

    logs

--- function-colour.sh
+++ function-colour.sh
@@ -29,9 +29,9 @@ mkdir -p $LOGS_FOLDER
 VALIDATE () {
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 installation - $R FAILED $C"
+        echo -e "$2 installation - $R FAILED $C" | tee -a $LOGS
     else
-        echo -e "$2 installation is $G SUCCESSFUL $C" 
+        echo -e "$2 installation is $G SUCCESSFUL $C" | tee -a $LOGS
     fi
 
 }
@@ -40,10 +40,10 @@ VALIDATE () {
 
 if [ $USERID -ne 0 ]
 then 
-    echo -e "$R You don't have root access, hence exiting the script $C"
+    echo -e "$R You don't have root access, hence exiting the script $C" | tee -a $LOGS
     exit 1 # to exit the script
 else
-    echo -e "$Y Proceeding to install the software $C"
+    echo -e "$Y Proceeding to install the software $C" | tee -a $LOGS
 
 fi
 
@@ -53,37 +53,37 @@ dnf list installed mysql &>>$LOGS
 
 if [ $? -ne 0 ]
 then
-    echo -e "$Y MYSQL is not installed, Hence installing it $C"
+    echo -e "$Y MYSQL is not installed, Hence installing it $C" | tee -a $LOGS
     dnf install mysql -y &>>$LOGS
     #3 Check if mysql installation is successul 
     VALIDATE $? "mysql"
 
 else 
-    echo -e "$G MYSQL is already installed... Hence skipping this step $C"
+    echo -e "$G MYSQL is already installed... Hence skipping this step $C" | tee -a $LOGS
 fi
     
 dnf list installed nginx &>>$LOGS
 
 if [ $? -ne 0 ]
 then
-    echo -e "$Y NGINX is not installed, Hence installing it $C"
+    echo -e "$Y NGINX is not installed, Hence installing it $C" | tee -a $LOGS
     dnf install nginx -y &>>$LOGS
     #3 Check if nginx installation is successul 
     VALIDATE $? "nginx"
 
 else 
-    echo -e "$G NGINX is already installed... Hence skipping this step $C"
+    echo -e "$G NGINX is already installed... Hence skipping this step $C" | tee -a $LOGS
 fi
 
 dnf list installed python3 &>>$LOGS
 
 if [ $? -ne 0 ]
 then
-    echo -e "$Y PYTHON3 is not installed, Hence installing it $C"
+    echo -e "$Y PYTHON3 is not installed, Hence installing it $C" | tee -a $LOGS
     dnf install python3 -y &>>$LOGS
     #3 Check if pyton3 installation is successul 
     VALIDATE $? "python3"
 
 else 
-    echo -e "$G python3 is already installed... Hence skipping this step $C"
+    echo -e "$G python3 is already installed... Hence skipping this step $C" | tee -a $LOGS
 fi
