6a488816d426ab9d6f82be09a581f36cbb3595fc
Kishore Singireddy <official4kishore@gmail.com>
2025-05-22 18:03:54 +0530

    logs

--- loops.sh
+++ loops.sh
@@ -2,14 +2,58 @@
 
 #Loops Practice
 
-for i in {1..20}
+echo "Script execution started at $(date)
+
+USERID=$(id -u)
+
+#Colours
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+C='\e[0m'
+
+#LOGS
+LOG_FOLDER="/var/log/shell_pract"
+LOG_SCRIPT=$(echo $0 | awk -F "." '{print $1F}')
+LOGS="$LOG_FOLDER/$LOG_SCRIPT.log"
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE () {
+
+if [ $1 -ne 0 ]
+then 
+    echo " Installation of $R $2 Failed $C" | tee -a $LOGS
+else 
+    echo "Installation of $G $2 is Successful $C" | tee -a $LOGS
+fi
+
+}
+
+SOFTWARE=("nginx" "mysql" "python3")
+
+for i in ${SOFTWARE[@]}
 do 
-    echo "$i"
+    if [ $USERID -ne 0 ]
+    then 
+        echo "$R You don not have root access, can not proceed to install $C "| tee -a $LOGS
+    else 
+        echo "$G You have root access, proceeding to install the software $C" | tee -a $LOGS
+    fi
+
+    dnf list installed $i &>>$LOGS
+
+    if [ $? -ne 0 ]
+
+    then 
+        echo " $Y Installing $i $C"
+        dnf install $i -y  &>>$LOGS
+        VALIDATE $1 $i 
+    else 
+        echo " $G $i is already installed, No action required" | tee -a $LOGS
+    fi
+
 done
 
-myfav=("UM" "SL" "SV" "MD" "MG")
 
-for i in ${myfav[@]}
-do 
-    echo "$i"
-done
