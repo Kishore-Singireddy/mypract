de1c1931be78cd0cf0bc7835f1f9047e75866e90
Kishore Singireddy <official4kishore@gmail.com>
2025-05-22 18:32:01 +0530

    logs

--- loops.sh
+++ loops.sh
@@ -5,45 +5,48 @@
 echo "Script execution started at $(date)"
 
 
+USERID=$(id -u)
+
+#Colours
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+C='\e[0m'
+
+#LOGS
+LOG_FOLDER="/var/log/shell_pract"
+LOG_SCRIPT=$(echo $0 | awk -F "." '{print $1F}')
+LOGS="$LOG_FOLDER/$LOG_SCRIPT.log"
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE () {
+
+if [ $1 -ne 0 ]
+then 
+    echo -e " Installation of $R $2 Failed $C" | tee -a $LOGS
+else 
+    echo -e "Installation of $G $2 is Successful $C" | tee -a $LOGS
+fi
+
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R You don not have root access, can not proceed to install $C " | tee -a $LOGS
+    exit 1
+else 
+    echo -e "$G You have root access, proceeding to install the software $C" | tee -a $LOGS
+fi
+
+
 
 SOFTWARE=("nginx" "mysql" "python3")
 
 for i in ${SOFTWARE[@]}
 do 
 
-    USERID=$(id -u)
-
-    #Colours
-    R='\e[31m'
-    G='\e[32m'
-    Y='\e[33m'
-    B='\e[34m'
-    C='\e[0m'
-
-    #LOGS
-    LOG_FOLDER="/var/log/shell_pract"
-    LOG_SCRIPT=$(echo $0 | awk -F "." '{print $1F}')
-    LOGS="$LOG_FOLDER/$LOG_SCRIPT.log"
-
-    mkdir -p $LOG_FOLDER
-
-    VALIDATE () {
-
-    if [ $1 -ne 0 ]
-    then 
-        echo -e " Installation of $R $2 Failed $C" | tee -a $LOGS
-    else 
-        echo -e "Installation of $G $2 is Successful $C" | tee -a $LOGS
-    fi
-
-    }
-
-    if [ $USERID -ne 0 ]
-    then 
-        echo -e "$R You don not have root access, can not proceed to install $C " | tee -a $LOGS
-    else 
-        echo -e "$G You have root access, proceeding to install the software $C" | tee -a $LOGS
-    fi
 
     dnf list installed $i &>>$LOGS
 
